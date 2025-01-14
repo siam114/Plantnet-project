ee141ee8209f2e0efa139fe83513058e05a1c75d
siam114 <smsiamislam598@gmail.com>
2025-01-14 19:45:28 +0600

    first commit

---
+++ client/src/pages/Dashboard/Seller/AddPlant.jsx
@@ -0,0 +1,73 @@
+import { Helmet } from "react-helmet-async";
+import AddPlantForm from "../../../components/Form/AddPlantForm";
+import { imageUpload } from "../../../api/utils";
+import useAuth from "../../../hooks/useAuth";
+import { useState } from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+
+const AddPlant = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure()
+  const [uploadImage, setUploadImage] = useState({image:{name:'Upload Button'}});
+  const [loading, setLoading] = useState(false)
+  //handle form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const form = e.target;
+    const name = form.name.value;
+    const description = form.description.value;
+    const category = form.category.value;
+    const price = parseFloat(form.price.value);
+    const quantity = parseInt(form.quantity.value);
+    const image = form.image.files[0];
+    const imageURL = await imageUpload(image);
+
+    //seller info
+    const seller = {
+      name: user?.displayName,
+      image: user?.photoURL,
+      email: user?.email,
+    };
+
+    //create plant data object
+    const plantData = {
+      name,
+      category,
+      description,
+      price,
+      quantity,
+      image: imageURL,
+      seller,
+    };
+
+    //save plant in db
+    try{
+      //post req
+      await axiosSecure.post('/plants', plantData)
+      toast.success('Data Added Successfully!')
+    }catch(err){
+      console.log(err)
+    }finally{
+      setLoading(false);
+    }
+  };
+  return (
+    <div>
+      <Helmet>
+        <title>Add Plant | Dashboard</title>
+      </Helmet>
+
+      {/* Form */}
+      <AddPlantForm
+        handleSubmit={handleSubmit}
+        uploadImage={uploadImage}
+        setUploadImage={setUploadImage}
+        loading={loading}
+      />
+    </div>
+  );
+};
+
+export default AddPlant;
---
+++ server/index.js
@@ -0,0 +1,130 @@
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const morgan = require('morgan')
+
+const port = process.env.PORT || 5000
+const app = express()
+// middleware
+const corsOptions = {
+  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
+
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2iff3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+async function run() {
+  try {
+    const db = client.db('plantNet-session')
+    const userCollection = db.collection('users')
+    const plantsCollection = db.collection('plants')
+
+    //save or update a user in db
+    app.post('/users/:email', async(req,res)=>{
+      const email = req.params.email;
+      const query = { email }
+      const user = req.body;
+      //check if user exists in db
+      const isExist = await userCollection.findOne(query)
+      if(isExist){
+        return res.send(isExist)
+      }
+      const result = await userCollection.insertOne({...user, role: 'customer', timestamp: Date.now()})
+      res.send(result)
+    })
+
+    // Generate jwt token
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === 'production',
+          sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        .send({ success: true })
+    })
+    // Logout
+    app.get('/logout', async (req, res) => {
+      try {
+        res
+          .clearCookie('token', {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+          })
+          .send({ success: true })
+      } catch (err) {
+        res.status(500).send(err)
+      }
+    })
+
+    //save a plant data in db
+    app.post('/plants', verifyToken, async(req,res)=>{
+      const plant = req.body
+      const result = await plantsCollection.insertOne(plant)
+      res.send(result)
+    })
+
+      //gets all plant data from  db
+      app.get('/plants', async(req,res)=>{
+        const result = await plantsCollection.find().toArray()
+        res.send(result)
+      })
+  
+
+    // Send a ping to confirm a successful connection
+    await client.db('admin').command({ ping: 1 })
+    console.log(
+      'Pinged your deployment. You successfully connected to MongoDB!'
+    )
+  } finally {
+    // Ensures that the client will close when you finish/error
+  }
+}
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello from plantNet Server..')
+})
+
+app.listen(port, () => {
+  console.log(`plantNet is running on port ${port}`)
+})
